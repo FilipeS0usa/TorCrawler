0142e85f227631eb01bab91ba8fba9b001674299
FilipeS0usa <filipes0usa.git@proton.me>
2024-11-23 19:08:17 +0000

    Update python code formating PEP8

--- src/crawler.py
+++ src/crawler.py
@@ -9,11 +9,10 @@ from functions.functions import *
 # Disable the logging of the SQLAlchemy
 logging.disable(logging.CRITICAL)
 
-if __name__ == '__main__':
-    
+if __name__ == "__main__":
     # This will be the first url of the database
     url_1 = "http://torlinksge6enmcyyuxjpjkoouw4oorgdgeo7ftnq3zodj7g2zxi3kyd.onion/"
-    
+
     # Create the Mongo session
     db_mongo = get_mongo_session()
 
@@ -22,28 +21,25 @@ if __name__ == '__main__':
 
     # create the tor session
     tor_session = get_tor_session()
-    
+
     # create model
     model = Model(db_sql, db_mongo, tor_session)
-    
+
     # create a view
     view = View()
-    
+
     # create a controller
     controller = Controller(model, view)
-    
+
     # Check if it is the first url in the database
     controller.first_url(url_1)
 
     # this is the scrapper
     while True:
-    
         controller.grab_oldest_domain()
-        
-        
 
 
-# TODO: Create function that serves as a debuger, if we receive an argument (like -v) it 
+# TODO: Create function that serves as a debuger, if we receive an argument (like -v) it
 #       activates debugging mode (it will show prints so we know where is the bug)
 
 # TODO: If the file is to big, zip it and store it in the local machine for later inspection
